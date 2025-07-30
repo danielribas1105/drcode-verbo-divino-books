104559acc09f7faef0c5bda4116c57a4963dc644
Daniel <danielribas1105@gmail.com>
2025-07-29 23:10:44 -0300

    feat(components): atualização style buttons

--- src/components/DRButton/index.tsx
+++ src/components/DRButton/index.tsx
@@ -1,18 +1,46 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-const PrimaryButton = styled.button`
-  background: #eb9800;
+export interface DRButtonProps {
+  text?: string;
+  type?: 'primary' | 'secundary';
+  onClick?: () => void;
+}
+
+const StyledButton = styled.button<DRButtonProps>`
+  background: ${(props: DRButtonProps) =>
+    props.type === 'primary' ? '#eb9800' : '#fff'};
   padding: 16px 32px;
   border: 2px solid #eb9800;
-  color: #fff;
+  color: ${(props: DRButtonProps) =>
+    props.type === 'primary' ? '#fff' : '#eb9800'};
   font-size: 20px;
   cursor: pointer;
-  &:hover {
-    background: #b87900;
-    border: 2px solid #b87900;
-  }
+  ${(props: DRButtonProps) =>
+    props.type === 'primary'
+      ? css`
+          &:hover {
+            background: #b87900;
+            border: 2px solid #b87900;
+          }
+        `
+      : css`
+          &:hover {
+            background: #fff;
+            border: 2px solid #b87900;
+            color: #b87900;
+          }
+        `}
 `;
-export const DRButton = () => {
-  return <PrimaryButton>Clique aqui</PrimaryButton>;
+
+export const DRButton = ({
+  text,
+  type = 'primary',
+  onClick,
+}: DRButtonProps) => {
+  return (
+    <StyledButton onClick={onClick} type={type}>
+      {text}
+    </StyledButton>
+  );
 };
--- stories/DRButton.stories.tsx
+++ stories/DRButton.stories.tsx
@@ -1,11 +1,23 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { DRButton } from '../src/components/DRButton';
+import { DRButton, DRButtonProps } from '../src/components/DRButton';
 
 export default {
   title: 'Components/DRButton',
   component: DRButton,
 } as ComponentMeta<typeof DRButton>;
 
-const Template: ComponentStory<typeof DRButton> = () => <DRButton />;
+const Template: ComponentStory<typeof DRButton> = args => (
+  <DRButton {...args} />
+);
 
 export const Primary = Template.bind({});
+Primary.args = {
+  text: 'Button Primary',
+  type: 'primary',
+} as DRButtonProps;
+
+export const Secundary = Template.bind({});
+Secundary.args = {
+  text: 'Button Secundary',
+  type: 'secundary',
+} as DRButtonProps;
